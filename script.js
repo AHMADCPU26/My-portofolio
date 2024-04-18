225a0ca3c699ff6dd3883db2a2788e5e9b0d110a
AHMADCPU26 <132383504+AHMADCPU26@users.noreply.github.com>
2024-04-18 18:09:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-=var sidebarOpen = false;
+// JavaScript code for toggling the sidebar when the button is clicked
+var sidebarOpen = false;
 var openSidebarBtn = document.getElementById("openSidebarBtn");
-var body = document.querySelector("body");
 
 openSidebarBtn.addEventListener("click", function() {
   toggleSidebar();
@@ -8,24 +8,29 @@ openSidebarBtn.addEventListener("click", function() {
 
 document.getElementById("homeButton").addEventListener("click", function() {
   window.location.href = "index.html";
+  toggleSidebar();
 });
 
 document.getElementById("aboutButton").addEventListener("click", function() {
   window.location.href = "about.html";
+  toggleSidebar();
 });
 
 document.getElementById("contactButton").addEventListener("click", function() {
   window.location.href = "contact.html";
+  toggleSidebar();
 });
 
 function toggleSidebar() {
   var sidebar = document.getElementById("sidebar");
 
   if (!sidebarOpen) {
-    body.classList.add("sidebar-open");
+    sidebar.style.left = '0';
+    openSidebarBtn.style.left = '250px'; // Move the button to the right when the menu appears
   } else {
-    body.classList.remove("sidebar-open");
+    sidebar.style.left = '-250px';
+    openSidebarBtn.style.left = '10px'; // Move the button back to its original position when the menu is closed
   }
 
   sidebarOpen = !sidebarOpen;
-}
+}\
